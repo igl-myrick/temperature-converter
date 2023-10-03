58d6caa8cf7f054ed25f556105af3c2f3c187013
igl-myrick <igl.myrick@gmail.com>
2023-10-03 15:58:34 -0700

    add calculation and result for fahrenheit conversion

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 function fToC(userNum) {
-  const convertedNum = userNum - 32 / (9 / 5);
-  return convertedNum;
+  const fConvertedNum = (userNum - 32) * (5 / 9);
+  return fConvertedNum;
 }
 
-const userNum = parseInt(prompt("Enter your temperature:"));
+function cToF(userNum) {
+  const cConvertedNum = (userNum * (9 / 5)) + 32;
+  return cConvertedNum;
+}
 
-window.alert(userNum + " Fahrenheit is equal to " + fToC(userNum).toFixed(2) + " Celsius.");
+const userNum = parseInt(prompt("Enter a temperature to convert:"));
+
+window.alert(userNum + " Fahrenheit is equal to " + fToC(userNum).toFixed(2) + " Celsius. " + userNum + " Celsius is equal to " + cToF(userNum).toFixed(2) + " Fahrenheit.");
